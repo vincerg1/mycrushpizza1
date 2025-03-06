84b970e90125d61ca06ec5aaa142494311c10b6f
vincerg1 <vincerg1@gmail.com>
2025-03-06 21:11:54 +0100

    Corrección: conexión a MySQL en Railway

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,11 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    host: process.env.MYSQLHOST,      // <- Railway proporciona esto
+    user: process.env.MYSQLUSER,      // <- Railway proporciona esto
+    password: process.env.MYSQLPASSWORD, // <- Railway proporciona esto
+    database: process.env.MYSQLDATABASE, // <- Railway proporciona esto
+    port: process.env.MYSQLPORT || 3306  // <- Railway proporciona esto
 });
 
 
