c4e5819ea20e22f7fe6ec5cd670d70a4400a840d
vincerg1 <vincerg1@gmail.com>
2025-03-06 16:20:36 +0100

    Configurando MySQL con DATABASE_URL en Railway

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
+if (!process.env.DATABASE_URL) {
+    console.error("❌ ERROR: La variable DATABASE_URL no está definida.");
+    process.exit(1);
+} else {
+    console.log("✅ DATABASE_URL detectada:", process.env.DATABASE_URL);
+}
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 5000;
 const app = express();
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
 
 
 app.use(cors());
