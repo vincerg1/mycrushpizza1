ba6e69db83585ed51c4a84108007f769ad56eab4
vincerg1 <vincerg1@gmail.com>
2025-11-23 13:43:48 +0100

    UPDATE 22NOV2025/1343HRS

--- frontend/src/GameCouponsGallery.js
+++ frontend/src/GameCouponsGallery.js
@@ -1,7 +1,7 @@
 // src/components/GameCouponsGallery.js
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import "./GameCouponsGallery.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp, faTiktok } from "@fortawesome/free-brands-svg-icons";
 import { faMobileScreenButton } from "@fortawesome/free-solid-svg-icons";
